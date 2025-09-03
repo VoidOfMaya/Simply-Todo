3d90f707103a015f96b4da3cf98b00d960ac75eb
VoidOfMaya <mayaofthevoid@gmail.com>
2025-09-03 08:50:38 -0700

    creats and orgenizes priority tabs on topnav

--- src/modules/DOM.js
+++ src/modules/DOM.js
@@ -6,10 +6,29 @@
  => task card elements
  */
 
- import { colorScheme } from "./colors"; // topwhite, mainwhite, balck
+ import { topWhite,mainWhite, black ,green, urgentRed, moderateYellow } from "./colors"; // topwhite, mainwhite, balck
 
- const color = new colorScheme();
- const body = document.querySelector("body");
+const body = document.querySelector("body");
+
+const topLogo = document.createElement("div");
+topLogo.innerHTML ="logo placeholder";
+topLogo.style.alignSelf = "center";
+topLogo.style.marginLeft = "2vw";
+topLogo.style.gridArea = "logo";
+
+const urgent = document.createElement("div");
+urgent.style.background = urgentRed();
+urgent.style.gridArea ="urgent";
+console.log(urgentRed());
+
+const upComing = document.createElement("div");
+upComing.style.background = moderateYellow();
+upComing.style.gridArea ="upcoming";
+
+const free = document.createElement("div");
+free.style.background = green();
+free.style.gridArea ="free";
+
  
 
  const onInit = (function(){
@@ -23,24 +42,39 @@
     `
 })();
 const topBar = function(){
+    
     const top = document.createElement("div");
-    top.style.backgroundColor = color.topWhite;
+    top.style.backgroundColor = topWhite();
     top.style.boxShadow = "inset 0px -18px 27px -35px rgba(29, 29, 31, 1)"
+    top.style.display = "grid";
+    top.style.gridTemplate = "1fr/ 47fr 1fr 1fr 1fr";
+   
+    top.appendChild(topLogo);
+    top.appendChild(urgent);
+    top.appendChild(upComing);
+    top.appendChild(free);
+    top.style.gridTemplateAreas = `"logo urgent upcoming free"`
+    //layout placement
     top.style.gridArea = "top";
     return top
 }
 const sideBar = function(){
     const side = document.createElement("div");
-    side.style.backgroundColor = color.black;
+    side.style.backgroundColor = black(); 
+    side.style.boxShadow = "inset -1px 30px 33px -9px rgba(0, 0, 0, 1)"
     side.style.gridArea = "side";
     return side
 }
 const displayContainer = function(){
     const display = document.createElement("div");
-    display.style.backgroundColor = color.mainWhite;
+    display.style.backgroundColor = mainWhite();
+    display.style.boxShadow = "inset 22px 28px 34px -9px rgba(255, 255, 255, 1)"
     display.style.gridArea = "main";
     return display
 }
+
+
+
 export{
     body,
     topBar,
--- src/modules/colors.js
+++ src/modules/colors.js
@@ -1,9 +1,20 @@
 
-class colorScheme{
-    topWhite ="#e5e5ea"
-    mainWhite ="#f5f5f7"
-    black ="#1d1d1f"    
+
+function topWhite(){return "#e5e5ea"};
+function mainWhite(){return "#f5f5f7"};
+function black(){return "#1D1D1F"} ; 
+function urgentRed(){return "linear-gradient(to bottom,rgb(255, 0, 0), rgb(255, 69, 0)"};
+function moderateYellow(){return 'linear-gradient(to bottom,rgb(243, 158, 0), #FFD700)'};
+function green(){return 'linear-gradient(to bottom,rgb(33, 173, 20), #3CB371)'};
+
+
+
+
+export {
+    urgentRed,
+    moderateYellow,
+    green,
+    black,
+    mainWhite,
+    topWhite,
 }
-
-
-export {colorScheme,}
